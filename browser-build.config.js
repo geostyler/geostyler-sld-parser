17a12aa7639066b581c9791f3de89d0bb5a35594
jansule <j_sule02@uni-muenster.de>
2018-10-15 10:56:34 +0200

    create browser build

---
+++ browser-build.config.js
@@ -0,0 +1,26 @@
+const webpack = require("webpack");
+module.exports = {
+  entry: "./src/SldStyleParser.ts",
+  output: {
+    filename: "sldStyleParser.js",
+    path: __dirname + "/browser",
+    library: "GeoStylerSLDParser"
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {
+        test: /\.ts$/,
+        include: /src/,
+        loader: "awesome-typescript-loader"
+      },
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+  ]
+};
