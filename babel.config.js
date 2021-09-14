be00b172f6b366480d015c01885d792e237e23f1
Kai Volland <volland@terrestris.de>
2021-09-14 12:08:17 +0200

    updates webpack

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   "presets": [
     ["@babel/env", {
-      "useBuiltIns": "usage"
+      "useBuiltIns": "usage",
+      "corejs": 3
     }],
     "@babel/preset-typescript"
   ],
