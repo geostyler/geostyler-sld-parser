398ce0ca844e17d06e37ebc3eb8099c1d71f454f
pprev94 <philippe.prevautel@ign.fr>
2024-06-21 13:28:57 +0200

    fix: offset Y for Textsymbolizer (#932)
    * fix: offset y for textsymbolizer #566
    
    * fix: few tests changed

--- data/styles/point_styledLabel_elementOrder.ts
+++ data/styles/point_styledLabel_elementOrder.ts
@@ -10,7 +10,7 @@ const pointStyledLabel: Style = {
       label: 'prefix: {{name}}{{title}} entity',
       font: ['Arial'],
       size: 12,
-      offset: [0, 5],
+      offset: [0, -5],
       haloColor: '#000000',
       haloWidth: 5,
       opacity: 1,
--- src/SldStyleParser.ts
+++ src/SldStyleParser.ts
@@ -826,7 +826,7 @@ export class SldStyleParser implements StyleParser<string> {
           const y = get(displacement, 'DisplacementY.#text');
           textSymbolizer.offset = [
             Number.isFinite(x) ? numberExpression(x) : 0,
-            Number.isFinite(y) ? numberExpression(y) : 0,
+            Number.isFinite(y) ? -numberExpression(y) : 0,
           ];
         }
         const rotation = get(pointPlacement, 'Rotation.#text');
@@ -2016,7 +2016,7 @@ export class SldStyleParser implements StyleParser<string> {
             }]
           }, {
             [DisplacementY]: [{
-              '#text': textSymbolizer.offset[1].toString()
+              '#text': (-textSymbolizer.offset[1]).toString()
             }]
           }]
         });
