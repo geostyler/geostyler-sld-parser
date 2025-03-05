c08a055d589b24fa01df0b2d7d077a56579438b1
Terry McDonell <35645950+terry-longmacch@users.noreply.github.com>
2025-03-05 17:29:13 +1100

    feat: add more wellknownnames

--- src/SldStyleParser.ts
+++ src/SldStyleParser.ts
@@ -1143,6 +1143,7 @@ export class SldStyleParser implements StyleParser<string> {
       case 'arrow':
       case 'arrowhead':
       case 'asterisk_fill':
+      case 'backslash':
       case 'circle':
       case 'cross':
       case 'cross2':
@@ -1170,6 +1171,7 @@ export class SldStyleParser implements StyleParser<string> {
       case 'rounded_square':
       case 'semi_circle':
       case 'shield':
+      case 'slash':
       case 'square':
       case 'square_with_corners':
       case 'star':
@@ -1188,6 +1190,13 @@ export class SldStyleParser implements StyleParser<string> {
       case 'shape://times':
       case 'shape://oarrow':
       case 'shape://carrow':
+      case 'brush://dense1':
+      case 'brush://dense2':
+      case 'brush://dense3':
+      case 'brush://dense4':
+      case 'brush://dense5':
+      case 'brush://dense6':
+      case 'brush://dense7':
         markSymbolizer.wellKnownName = wellKnownName;
         break;
       default:
